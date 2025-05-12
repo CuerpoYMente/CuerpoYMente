898abe29acf9ceb8721ffcd243f9f6e8c7bdec22
Francisco Torres Peñalver <ftp00007@red.ujaen.es>
2025-05-12 09:02:09 +0200

    add vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👇 IMPORTANTE: base: '' y configuración para fallback
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false
+        secure: false,
       }
     }
+  },
+  build: {
+    outDir: 'dist'
   }
 })
